a06dc7647174e2c05768926240eb307e3b7a1ef5
Manish Pardhan <manish@instaastro.in>
2025-07-28 21:46:51 +0530

    final commit

--- app/README.md
+++ app/README.md
@@ -10,3 +10,27 @@ Currently, two official plugins are available:
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+{
+	"price": 0.1, // This refers to price of each NFT
+	"number": 3, // Number of the NFT we want to mint
+	"gatekeeper": null,
+	"solTreasuryAccount": "", // This should be our own wallet address
+    // When the NFT will sold the money will be transfer to the soltreasuryaccount
+	"splTokenAccount": null,
+	"splToken": null,
+	"goLiveDate": "05 Jan 2021 00:00:00 GMT", // When the minting of the nft will start
+	"endSettings": null,
+	"whitelistMintSettings": null,
+	"hiddenSettings": null,
+	"storage": "arweave", // where we want to store the NFTs
+	"ipfsInfuraProjectId": null,
+	"ipfsInfuraSecret": null,
+	"awsS3Bucket": null,
+	"noRetainAuthority": false,
+	"noMutable": false
+}
+
+REACT_APP_CANDY_MACHINE_ID=<YOUR CANDY MACHINE ID GOES HERE>
+REACT_APP_SOLANA_NETWORK=devnet
+REACT_APP_SOLANA_RPC_HOST=https://explorer-api.devnet.solana.com
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import twitterLogo from "./assets/twitter-logo.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
+// Constants
+const TWITTER_HANDLE = "_buildspace";
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null);
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana?.isPhantom) {
+          console.log('Phantom Wallet is Found!');
+          const res = await solana.connect({
+            onlyIfTrusted: true
+          })
+          setWalletAddress(res?.publickKey?.toString())
+        }
+      } else {
+        console.log('Solana object not found! Get a Phantom wallet!')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const connectWallet = async () => {
+    const { solana } = window;
+    if(solana) {
+      const response = await solana.connect();
+      console.log(`Connected with key: ${response?.publickKey?.toString()}`)
+    }
+  }
+
+  const renderNotConnectedContainer = () => {
+    return(
+      <button className="cta-button connect-wallet-button" onClick={connectWallet}>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header">🍭 Candy Drop</p>
+          <p className="sub-text">NFT drop machine with fair mint</p>
+          {!walletAddress && renderNotConnectedContainer()}
+        </div>
+        <div className="footer-container">
+          <img
+            alt="Twitter Logo"
+            className="twitter-logo"
+            src={twitterLogo}
+          />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`Adapted from @${TWITTER_HANDLE}`}</a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
